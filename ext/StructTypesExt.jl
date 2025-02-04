e22d29eddfb6e6983b2d970ff9cb1d2a34213d30
TEC <git@tecosaur.net>
2025-02-05 00:53:30 +0800

    Remove boilerplate in StructTypes extension

--- ext/StructTypesExt.jl
+++ ext/StructTypesExt.jl
@@ -6,34 +6,9 @@ module StructTypesExt
 using AcademicIdentifiers
 using StructTypes
 
-StructTypes.StructType(::Type{ArXiv}) = StructTypes.StringType()
-StructTypes.construct(::Type{ArXiv}, arxiv::String; _kw...) = ArXiv(arxiv)
-
-StructTypes.StructType(::Type{ArXivOld}) = StructTypes.StringType()
-StructTypes.construct(::Type{ArXivOld}, arxiv::String; _kw...) = ArXivOld(arxiv)
-
-StructTypes.StructType(::Type{DOI}) = StructTypes.StringType()
-StructTypes.construct(::Type{DOI}, doi::String; _kw...) = DOI(doi)
-
-StructTypes.StructType(::Type{ORCID}) = StructTypes.StringType()
-StructTypes.construct(::Type{ORCID}, orcid::String; _kw...) = ORCID(orcid)
-
-StructTypes.StructType(::Type{ROR}) = StructTypes.StringType()
-StructTypes.construct(::Type{ROR}, ror::String; _kw...) = ROR(ror)
-
-StructTypes.StructType(::Type{PMID}) = StructTypes.StringType()
-StructTypes.construct(::Type{PMID}, pmid::String; _kw...) = PMID(pmid)
-
-StructTypes.StructType(::Type{PMCID}) = StructTypes.StringType()
-StructTypes.construct(::Type{PMCID}, pmcid::String; _kw...) = PMCID(pmcid)
-
-StructTypes.StructType(::Type{ISSN}) = StructTypes.StringType()
-StructTypes.construct(::Type{ISSN}, issn::String; _kw...) = ISSN(issn)
-
-StructTypes.StructType(::Type{ISBN}) = StructTypes.StringType()
-StructTypes.construct(::Type{ISBN}, isbn::String; _kw...) = ISBN(isbn)
-
-StructTypes.StructType(::Type{Wikidata}) = StructTypes.StringType()
-StructTypes.construct(::Type{Wikidata}, wikidata::String; _kw...) = Wikidata(wikidata)
+for idtype in (ArXiv, ArXivOld, DOI, ORCID, ROR, PMID, PMCID, ISSN, ISBN, Wikidata)
+    @eval StructTypes.StructType(::Type{$idtype}) = StructTypes.StringType()
+    @eval StructTypes.construct(::Type{$idtype}, id::String; _kw...) = $idtype(id)
+end
 
 end
